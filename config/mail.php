ac2fbf2f44cf1d475376afb88e3e12e749b257e3
DevelopingJeremy <quesadajeremy7@gmail.com>
2025-07-26 13:11:29 -0600

    Se añade la informacion del mail.php para trabajar

--- config/mail.php
+++ config/mail.php
@@ -1,9 +1,9 @@
 <?php
 
 // Datos para la conexión del Email
-$hostMail = ""; // Host del email
-$usernameMail = ""; // Correo desde que enviamos
-$passMail = ""; // Contraseña del email
+$hostMail = "smtp.hostinger.com";
+$usernameMail = "info@covomosa.app";
+$passMail = "M3a~mbU[";
 $SMTPSecure = "ssl";
 $PortMail = 465;
 
